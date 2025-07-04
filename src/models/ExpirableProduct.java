a73e595de2a4e5648f706db4eea5210ba9700557
abdelrahmann22 <abdelrahmannn202@gmail.com>
2025-07-04 17:08:14 +0300

    model: expirableproduct

---
+++ src/models/ExpirableProduct.java
@@ -0,0 +1,19 @@
+package models;
+import interfaces.Expirable;
+
+import java.time.LocalDate;
+
+public class ExpirableProduct extends Product implements Expirable{
+    private LocalDate expiryDate;
+
+    public ExpirableProduct(String name, double price, int quantity, LocalDate expiryDate){
+        super(name, price, quantity);
+        this.expiryDate = expiryDate;
+
+    }
+
+    @Override
+    public LocalDate getExpiryDate(){return expiryDate;}
+    @Override
+    public boolean isExpired(){ return LocalDate.now().isAfter(expiryDate);}
+}
