514f52fecd5dc7832092cd103ae13d19634d0019
abdelrahmann22 <abdelrahmannn202@gmail.com>
2025-07-04 16:29:04 +0300

    model: product

---
+++ src/models/Product.java
@@ -0,0 +1,28 @@
+package models;
+
+public abstract class Product {
+    protected String name;
+    protected double price;
+    protected int quantity;
+
+    public Product(String name, double price, int quantity){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {return name;}
+    public double getPrice() {return price;}
+    public int getQuantity() {return quantity;}
+
+    public boolean isAvailable(int quantity){
+        return this.quantity >= quantity;
+    }
+
+    public void reduceQuantity(int value){
+        this.quantity = value;
+    }
+
+    public abstract boolean isExpired();
+    public abstract boolean isShippable();
+}
