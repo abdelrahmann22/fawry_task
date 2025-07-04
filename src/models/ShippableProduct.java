de2901cd2af2c3cb0e0cbbf7b71961fac5268ece
abdelrahmann22 <abdelrahmannn202@gmail.com>
2025-07-04 17:08:48 +0300

    model: shippableproduct

---
+++ src/models/ShippableProduct.java
@@ -0,0 +1,15 @@
+package models;
+import interfaces.Shippable;
+public class ShippableProduct extends Product implements Shippable{
+    private double weight;
+
+    public ShippableProduct(String name, double price, int quantity, double weight){
+        super(name, price, quantity);
+        this.weight = weight;
+    }
+
+    @Override
+    public String getName(){return name;}
+    @Override
+    public double getWeight(){return weight;}
+}
